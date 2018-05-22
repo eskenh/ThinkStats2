6a5ec53da8520073491db096976326bda5168c0e
eskenh <tp_108@bk.ru>
2018-05-22 19:35:04 +0200

    exercise 02

--- code/chap02ex.py
+++ code/chap02ex.py
@@ -13,6 +13,29 @@ from operator import itemgetter
 import first
 import thinkstats2
 
+def CohenEffectSize(group1, group2):
+    """Computes Cohen's effect size for two groups.
+    
+    group1: Series or DataFrame
+    group2: Series or DataFrame
+    
+    returns: float if the arguments are Series;
+             Series if the arguments are DataFrames
+    """
+    diff = group1.mean() - group2.mean()
+
+    var1 = group1.var()
+    var2 = group2.var()
+    n1, n2 = len(group1), len(group2)
+
+    pooled_var = (n1 * var1 + n2 * var2) / (n1 + n2)
+    d = diff / np.sqrt(pooled_var)
+    return d
+
+def FirstsHeavier(firsts, others):
+    print("First babies are heavier?")
+    print (firsts.totalwgt_lb.mean(), others.totalwgt_lb.mean())
+    print(CohenEffectSize(firsts.totalwgt_lb, others.totalwgt_lb))
 
 def Mode(hist):
     """Returns the value with the highest frequency.
@@ -21,7 +44,9 @@ def Mode(hist):
 
     returns: value from Hist
     """
-    return 0
+    f, p = max([(f, p) for p, f in hist.Items()])
+    print(f,p)
+    return p
 
 
 def AllModes(hist):
@@ -31,7 +56,7 @@ def AllModes(hist):
 
     returns: iterator of value-freq pairs
     """
-    return []
+    return [(p, f) for p, f in hist.Items()]
 
 
 def main(script):
@@ -49,11 +74,13 @@ def main(script):
 
     # test AllModes
     modes = AllModes(hist)
+    #print(modes)
     assert modes[0][1] == 4693, modes[0][1]
 
     for value, freq in modes[:5]:
         print(value, freq)
 
+    FirstsHeavier(firsts, others)
     print('%s: All tests passed.' % script)
 
 
