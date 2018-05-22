86597a4400679440f0573b5e8d41c4f724c04fd8
eskenh <tp_108@bk.ru>
2018-05-22 11:25:58 +0200

    exercise 01

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -6,6 +6,7 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
 from __future__ import print_function
+from collections import defaultdict
 
 import numpy as np
 import sys
@@ -13,12 +14,28 @@ import sys
 import nsfg
 import thinkstats2
 
+def ReadAndCompare():
+    resp = ReadFemResp()
+    preg = ReadFemPreg()
+    
+    pregTimes = defaultdict(list)
+    for index, caseid in preg.caseid.iteritems():
+        pregTimes[caseid].append(index)
+
+    print(resp.pregnum.value_counts(sort=False))
+    
+    caseid = 2298
+    print(len(pregTimes[caseid]))
+    print(resp[resp.caseid == caseid].pregnum.values)
+    
+    return df
 
 def main(script):
     """Tests the functions in this module.
-
+    
     script: string script name
     """
+    ReadAndCompare()
     print('%s: All tests passed.' % script)
 
 
